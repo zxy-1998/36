1727e39f01fe456b14763fcfcdad74398b63117a
zxy-1998 <2071493753@qq.com>
2019-08-14 11:33:41 +0800

    test

---
+++ js/ES6/model/a.js
@@ -0,0 +1,4 @@
+var age = 10
+var name = "小红"
+export {age,name}
+export default age
---
+++ js/ES6/model/b.js
@@ -0,0 +1,11 @@
+import { age,name } from "/.a.js" 
+import  age from "/.a.js" 
+export default {
+  data () {
+    return { }
+  },
+  created:function(){
+  console.log(name)
+ console.log(age)
+ }
+}
